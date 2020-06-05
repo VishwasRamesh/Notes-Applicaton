f3d458407ecdcda6aa7f3e995d4107f3d42f61d9
Vishwas Ramesh <cuttingedge6034@gmail.com>
2020-06-05 08:14:17 +0530

    Give funtionality to add command

--- app.js
+++ app.js
@@ -19,9 +19,22 @@ yargs.version('1.1.0');
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function(){
-        console.log("Adding a new note");
-    }
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log("Title : ", argv.title);
+        console.log("Body : ", argv.body);
+    }    
 });
 
 //create remove command
@@ -54,4 +67,5 @@ yargs.command({
 
 const getNotes = require('./notes');
 
-console.log(yargs.argv);
+//Parses the arguments with the requierd configure
+yargs.parse();
